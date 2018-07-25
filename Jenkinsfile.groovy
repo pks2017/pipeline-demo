9f0830ae26616798fca05443684befd52f65fe1b
Praveen Singh <psingh@r4.co>
2018-07-25 18:44:58 +0530

    groovy file

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,31 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Build') {
+            steps {
+                echo 'Building..'
+				git credentialsId: '486f2c77-dfb7-4a1f-9f75-cb8623d36754', url: 'https://github.com/pks2017/pipeline-demo.git'
+            }
+        }
+        
+        stage('Deploy') {
+            steps {
+                echo 'Deploying....'
+            }
+        }
+        
+        stage('Test') {
+            steps {               
+				echo 'Testing..'
+				bzt 'ApiTesting/Uber/TestSuits/test-dev-backend-uber-Suite-Smoke.yml'
+            }
+        }
+        
+        stage('Release') {
+            steps {
+                echo 'Releasing to production..'
+            }
+        }        
+    }
+}
